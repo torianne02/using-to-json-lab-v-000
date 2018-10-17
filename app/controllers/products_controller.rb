6b835f5163d85a82c35ecdda89e83fa5509dc0b5
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-17 21:46:50 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,14 +22,13 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
-  def shows
+  def show
     @product = Product.find(params[:id])
     respond_to do |format|
       format.html { render :show }
       format.json { render json: @product.to_json(only: [:id, :name, :description, :inventory, :price])}
     end
   end
-
 
   private
 
