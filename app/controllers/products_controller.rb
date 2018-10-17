4acbb62551fe41ad69bdb37162c7146db30eabec
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-17 21:46:42 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,11 +30,6 @@ class ProductsController < ApplicationController
     end
   end
 
-  # delete this to incorporate it into posts#show
-  # def data
-  #   product = Product.find(params[:id])
-  #   render json: product.to_json(only: [:id, :name, :description, :inventory, :price])
-  # end
 
   private
 
